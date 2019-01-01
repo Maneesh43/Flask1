f88d0032f104b15cc9d0be905c892bed929411ce
Maneesh Thouti <33095871+Maneesh43@users.noreply.github.com>
2019-01-01 10:52:32 +0530

    Add files via upload

---
+++ fl1.py
@@ -0,0 +1,19 @@
+from flask import Flask,render_template,request,redirect,url_for
+app = Flask(__name__)
+@app.route('/success/<name>')
+def success(name):
+	#return render_template('success.html',name=user)
+	return render_template('success.html',name=name)
+@app.route('/')
+def index():
+	return render_template('log1.html')
+@app.route('/check1',methods=['POST','GET'])
+def check1():
+	if request.method =='POST':
+		user = request.form['nm']
+		return redirect(url_for('success',name=user))
+	else:
+		user = request.args.get('nm')
+		return redirect(url_for('success',name=user))
+if __name__ == '__main__':
+        app.run()
